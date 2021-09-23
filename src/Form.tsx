ff99bc1e76a9635992a8b83f6f85624366c596e1
takurinton <takurinton@takurinton.com>
2021-09-23 12:10:11 +0900

    fix: set undefined

--- src/Form.tsx
+++ src/Form.tsx
@@ -14,18 +14,20 @@ export const AnalyticsForm = () => {
 export const useForm = () => {
   const [state, setState] = useState<{
     domain: string | undefined;
-    path: string;
+    path: string | undefined;
     start: string;
     end: string;
   }>({
     domain: undefined, 
-    path: '',
+    path: undefined,
     start: '',
     end: '',
   });
 
   const handleChange = (eve: React.ChangeEvent<HTMLSelectElement>) => {
     const _state = { ...state, [eve.target.name]: eve.target.value }
+    if (_state.domain === 'undefined') _state.domain = undefined;
+    if (_state.path === '') _state.path = undefined;
     setState(_state);
     console.log(_state)
   };
