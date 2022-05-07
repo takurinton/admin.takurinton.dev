9a8bf588becb636b78321b44abfffe39ddcb470a
takurinton <takurinton@takurinton.dev>
2022-05-07 14:19:44 +0900

    add: editor

--- src/pages/BlogEdit/internal/Form.tsx
+++ src/pages/BlogEdit/internal/Form.tsx
@@ -1,42 +1,49 @@
-import { Flex, Input, TextField, ToggleButton, Typography } from "ingred-ui";
-import { ChangeEvent, createRef, useEffect } from "react";
+import { Button, Flex, Input, ToggleButton, Typography } from "ingred-ui";
+import { marked } from "marked";
+import { ChangeEvent } from "react";
 import styled from "styled-components";
 import { useForm } from "./hooks/useForm";
+import { markdownStyle } from "./markdownRenderer";
+import { renderStringToHtml } from "./renderStringToJSX";
 
 const EditorContainer = styled.div``;
+
+const ContentContainer = styled(Flex)`
+  padding: ${({ theme }) => theme.spacing * 2}px 0;
+`;
 
 const EditColumnContainer = styled.div`
   margin: 0% 2% 0;
   width: 46%;
 `;
 
-const TextAreaStyle = styled.div`
-  &:focus {
-    border: red;
-  }
-`;
+// const TextAreaStyle = styled.div`
+//   &:focus {
+//     border: red;
+//   }
+// `;
 
-const TextArea = ({
-  contents,
-  onChange,
-}: {
-  contents: string;
-  onChange: (name: string, newValue: string) => void;
-}) => {
-  const ref = createRef<HTMLDivElement>();
+// const TextArea = ({
+//   contents,
+//   onChange,
+// }: {
+//   contents: string;
+//   onChange: (name: string, newValue: string) => void;
+// }) => {
+//   const ref = createRef<HTMLDivElement>();
 
-  const handleKeydown = (event: KeyboardEvent) => {
-    if (event.key === "a") {
-      document.execCommand("delete");
-    }
-  };
+//   const handleKeydown = (event: KeyboardEvent) => {
+//     if (event.key === "a") {
+//       document.execCommand("delete");
+//     }
+//   };
 
-  return (
-    <TextAreaStyle ref={ref} contentEditable onKeyDown={handleKeydown}>
-      {contents}
-    </TextAreaStyle>
-  );
-};
+//   return (
+//     <TextAreaStyle ref={ref} contentEditable onKeyDown={handleKeydown}>
+//       {contents}
+//     </TextAreaStyle>
+//   );
+// };
 
 export const BlogEditForm = ({
   id,
@@ -60,14 +67,24 @@ export const BlogEditForm = ({
   });
 
   const getHight = (value: string) => {
-    return value.split("\n").length;
+    return `${value.split("\n").length}px`;
   };
 
   return (
     <EditorContainer>
-      <Flex display="flex">
+      <ContentContainer display="flex" style={{ alignItems: "center" }}>
+        <EditColumnContainer>{/* Empty */}</EditColumnContainer>
+        <EditColumnContainer>
+          <Flex style={{ width: "150px", margin: "0 0 0 auto" }}>
+            <Button>{state.open ? "保存" : "下書き保存"}</Button>
+          </Flex>
+        </EditColumnContainer>
+      </ContentContainer>
+      <ContentContainer display="flex" style={{ alignItems: "center" }}>
         <EditColumnContainer>
           <Input
+            width="100%"
+            placeholder="タイトル"
             value={state.title}
             name="title"
             onChange={({ target }: ChangeEvent<HTMLInputElement>) => {
@@ -76,27 +93,39 @@ export const BlogEditForm = ({
           />
         </EditColumnContainer>
         <EditColumnContainer>
-          <Typography>{state.title}</Typography>
+          <Typography component="h1" size="xxxxl" weight="bold">
+            {state.title}
+          </Typography>
         </EditColumnContainer>
-      </Flex>
-      <Flex display="flex">
+      </ContentContainer>
+      <ContentContainer display="flex">
         <EditColumnContainer>
-          <TextArea contents={state.contents} onChange={handleChange} />
-          {/* <Input
+          {/* <TextArea contents={state.contents} onChange={handleChange} /> */}
+          <Input
             multiline
+            width="100%"
             value={state.contents}
             name="contents"
+            placeholder="本文"
             height={getHight(state.contents)}
             onChange={({ target }: ChangeEvent<HTMLInputElement>) => {
               handleChange("contents", target.value);
             }}
-          ></Input> */}
+          ></Input>
         </EditColumnContainer>
         <EditColumnContainer>
-          <Typography>{state.contents}</Typography>
+          {renderStringToHtml(
+            marked.parse(state.contents, {
+              renderer: markdownStyle(),
+              // syntax highlight は管理画面にはいらないでしょw
+              // highlight: (code, lang) => {
+              //   return highlightjs.highlightAuto(code, [lang]).value;
+              // },
+            })
+          )}
         </EditColumnContainer>
-      </Flex>
-      <Flex display="flex">
+      </ContentContainer>
+      <ContentContainer display="flex">
         <EditColumnContainer>
           <ToggleButton
             active={state.open}
@@ -110,7 +139,7 @@ export const BlogEditForm = ({
         <EditColumnContainer>
           <Typography>{state.open ? "公開" : "非公開"}</Typography>
         </EditColumnContainer>
-      </Flex>
+      </ContentContainer>
       <Typography>{JSON.stringify(pub_date)}</Typography>
     </EditorContainer>
   );
